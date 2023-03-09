e508586265cea2f02bc93bdeb55ea8a418d1b6f7
Faranheit15 <ffaranm15@gmail.com>
2023-03-09 19:37:49 +0530

    styling cards in rn complete

--- components/FancyCard.tsx
+++ components/FancyCard.tsx
@@ -1,14 +1,81 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
 const FancyCard = () => {
   return (
     <View>
-      <Text>Fancy Card</Text>
+      <Text style={styles.headingText}>Some Image Shit</Text>
+      <View style={[styles.card, styles.cardElevated]}>
+        <Image
+          source={{
+            uri: 'https://images.pexels.com/photos/1603650/pexels-photo-1603650.jpeg?cs=srgb&dl=pexels-sudipta-mondal-1603650.jpg&fm=jpg',
+          }}
+          style={styles.cardImage}
+        />
+        <View style={styles.cardBody}>
+          <Text style={styles.cardTitle}>The Taj Mahal</Text>
+          <Text style={styles.cardLabel}>Agra, Uttar Pradesh</Text>
+          <Text style={styles.cardDescription}>The Taj Mahal is an Islamic ivory-white marble mausoleum on the right bank of the river Yamuna in Agra, Uttar Pradesh, India.</Text>
+          <Text style={styles.cardFooter}>12 mins away</Text>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default FancyCard
+export default FancyCard;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+  },
+  card: {
+    height: 360,
+    width: 380,
+    borderRadius: 6,
+    marginVertical: 12,
+    marginHorizontal: 16,
+  },
+  cardElevated: {
+    backgroundColor: '#ffffff',
+    elevation: 3,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+  },
+  cardImage: {
+    height: 200,
+    marginBottom: 8,
+    borderTopLeftRadius: 6,
+    borderTopRightRadius: 6
+  },
+  cardBody: {
+    flex: 1,
+    flexGrow: 1,
+    paddingHorizontal: 12
+  },
+  cardTitle: {
+    color: '#000000',
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 4
+  },
+  cardLabel: {
+    color: '#000000',
+    fontSize: 14,
+    marginBottom: 6
+  },
+  cardDescription: {
+    color: '#242B2E',
+    fontSize: 12,
+    marginBottom: 12,
+    marginTop: 6,
+    flexShrink: 1
+  },
+  cardFooter: {
+    color: '#000000',
+  },
+});
